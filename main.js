dd56bf88a335e672d6391362f8742eb58c50cafc
Elena <e.cotar327@gmail.com>
2021-04-30 15:02:49 +0300

    Changing console log with return, deleting global variables

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-const numOne = '';
-const numTwo = '';
-const numThree = '';
-
 const productSign = (numOne, numTwo, numThree) => {
-    const numProduct = numOne * numTwo * numThree;
+    let numProduct = numOne * numTwo * numThree;
     if (numProduct >= 0) {
-        console.log('The sign is +');
+        return 'The sign is +';
     } else {
-        console.log('The sign is -');
+        return 'The sign is -';
     }
 }
 
-console.log(productSign(-10, 2, -4));
+console.log(productSign(-10, -2, -4));
